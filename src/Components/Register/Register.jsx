b0bb44e74de5416db3adb1955da0ea3775ad1b07
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-10-06 18:11:35 +0600

    module 2

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,6 +1,18 @@
 
 
 const Register = () => {
+
+    const handleRegister=e=> {
+        e.preventDefault();
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        const name=e.target.name.value;
+
+
+        console.log(name, email,password)
+    }
+
+
     return (
         <div className="w-3/4 mx-auto">
             <div className=" flex flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none justify-center items-center">
@@ -8,7 +20,7 @@ const Register = () => {
                 <p className="mt-1 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
                     Enter your details to register.
                 </p>
-                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                <form onSubmit={handleRegister} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-4 flex flex-col gap-6">
                         <div className="relative h-11 w-full min-w-48">
                             <input className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" placeholder=" " />
